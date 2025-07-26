4014ba40e781caaacb846de686df2753904b5579
Oseagwina Excellence <oseagwinaexcellence@gmail.com>
2025-07-26 12:41:10 +0100

    commit

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+//Intro
 console.log('Hello World!');
+console.log('Hello!, Excellence');
+console.log('Excellence', '23', 'October')
+console.log('Excellence' + '23' + 'October');
+console.log('Excellence ' + '23 ' + 'October');
