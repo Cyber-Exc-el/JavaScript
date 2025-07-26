a2a5e7c226e346e8574bfaf893dfe5ee9afcef2c
Oseagwina Excellence <oseagwinaexcellence@gmail.com>
2025-07-26 13:13:44 +0100

    commit

--- variables.js
+++ variables.js
@@ -10,3 +10,18 @@ let _height;
 let _weight;
 console.log(_height);
 console.log(_weight);
+
+
+
+
+//
+let hheight = 12;
+let anotherheight = hheight;
+console.log(hheight);
+console.log(anotherheight);
+let wweight = 20
+console.log(wweight);
+let anotherweight = wweight;
+console.log(anotherweight);
+let aanotherweight = anotherweight;
+console.log(aanotherweight);
