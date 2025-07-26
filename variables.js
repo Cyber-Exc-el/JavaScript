6beace4643546ad91bc148fc8bd0220990416dab
Oseagwina Excellence <oseagwinaexcellence@gmail.com>
2025-07-26 14:23:58 +0100

    commit

--- variables.js
+++ variables.js
@@ -1,7 +1,4 @@
-//Variables
-
-"use  strict"; 
-
+/*Variables*/
 //original javascript syntax
 var height;
 var weight;
@@ -14,10 +11,7 @@ let _weight;
 console.log(_height);
 console.log(_weight);
 
-
-
-
-//*  /
+//
 let hheight = 12;
 let anotherheight = hheight;
 console.log(hheight);
@@ -31,8 +25,6 @@ console.log(aanotherweight);
 console.log('height');
 console.log('weight');
 
-
-
 //changing variables
 let red = 1;
 console.log(red);
@@ -41,8 +33,7 @@ console.log(red);
 red = red + 100;
 console.log(red);
 
-
-
+//Typed
 let greeting = "Hello!";
 let counter = 100;
 console.log(counter)
@@ -50,3 +41,31 @@ console.log(greeting);
 console.log(greeting);
 greeting = greeting + counter;
 console.log(greeting);
+
+
+//constant
+const gree_ting = "hello world!";
+console.log(gree_ting)
+
+
+let height_ = 20;
+let weight_ = 30
+{
+     let weight_ = 10;
+     {
+          console.log(height_ + weight_);
+     }
+     
+}
+console.log(height_ + weight_);
+
+var  _he_ight  =  180;
+{
+         var  _we_ight  =  70;
+       {
+          console.log(_he_ight);  
+          console.log(_we_ight);
+       }  
+}
+console.log(_he_ight);  
+console.log(_we_ight); 
