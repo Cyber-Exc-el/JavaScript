ff70976d6ae68ff4d36b14e706741825d6c64c34
Oseagwina Excellence <oseagwinaexcellence@gmail.com>
2025-07-31 13:51:57 +0100

    commit

---
+++ opeartors.js
@@ -0,0 +1,19 @@
+let a = 10;
+console.log(a);
+console.log(a++);
+console.log(a);
+
+let b = 11;
+console.log(b);
+console.log(++b);
+console.log(b);
+
+let c = 12;
+console.log(c);
+console.log(c--);
+console.log(c);
+
+let d = 13;
+console.log(d);
+console.log(d--);
+console.log(d);
